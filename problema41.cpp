2cbe7bd2de7d1dfbddcfe823e20e03a18020d9b7
Trifan David <trifand851@gmail.com>
2025-11-12 20:44:31 +0200

    problema 41 din document

---
+++ problema41.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int a[100][100],n;
+int main()
+{
+    cin>>n;
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            a[i][j] = i+j;
+        }
+
+    }
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+            cout<<a[i][j]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
